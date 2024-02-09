5c295d3bb0d9db7ba9516c17f5adf8775036e0dd
David <Darevalod-2022062@kinal.edu.gt>
2024-02-09 11:58:50 -0600

    Se arregla errores y sube collections como rar

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -8,25 +8,31 @@ const Login = require('../models/login');
 const loginGet = async (req, res = response) => {
     const { correo, password } = req.body;
     const query = { estado: true };
+    var comproba = false;
     const [usuarios] = await Promise.all([
         Usuario.find(query)
     ]);
 
     usuarios.forEach(element => {
+        
         const decryptedPass = cryptr.decrypt(element.password);
         if (element.correo == correo && decryptedPass == password) {
             //TIENE Accesso
-            res.status(200).json({
-                msg: `SE INICIO SESION, BIENVENIDO ${element.nombre}` 
-            });
-        }else{
-            res.status(400).json({
-                msg: `TUS DATOS NO COINCIDEN CON NADA EN LA BASE DE DATOS` 
-            });
+            comproba = true;
         }
     });
 
-    
+    if (comproba == true) {
+        res.status(200).json({
+            msg: `SE INICIO SESION, BIENVENIDO`
+        });
+    } else {
+        res.status(400).json({
+            msg: `TUS DATOS NO COINCIDEN CON NADA EN LA BASE DE DATOS`
+        });
+    }
+
+
 }
 
 module.exports = {
